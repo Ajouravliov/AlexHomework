86caf06d5fa02c7a4cf478bfc38830a6dce904f4
AJ <graudu@hotmail.com>
2018-06-19 21:22:44 +0300

    GreaterThanAverage added

---
+++ src/GreaterThanAverage.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class GreaterThanAverage {
+
+    public static void main(String[] args) {
+
+        int length = 10, sum = 0, average = 0;
+
+        int[] numbers = new int[length];
+        Random random = new Random();
+
+        for (int i =0; i<length ;i++) {
+
+            numbers [i] = random.nextInt(100);
+            sum = sum + numbers [i];
+        }
+
+        System.out.println("Here is an array : " + numbers.toString());
+
+        average = sum /length;
+        System.out.println ("Average of array is " + average);
+        System.out.println("Here are the numbers greater than average : ");
+
+        for ( int i = 0 ; i<length ; i++){
+
+            if (numbers[i] > average){
+
+                System.out.println (numbers[i]);
+            }
+        }
+    }
+}
