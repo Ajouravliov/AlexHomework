f2894284ae9dc306f10e0d2183fb75f0bc8d7fed
AJ <graudu@hotmail.com>
2018-08-13 01:37:19 +0300

    Gravetrips_05

--- src/Gravetrips/GameGravetrips.java
+++ src/Gravetrips/GameGravetrips.java
@@ -5,8 +5,13 @@ public class GameGravetrips {
     public static void main(String[] args) {
 
         GameBoard gameBoard = new GameBoard();
+        ComputerPlayer firstPlayer = new ComputerPlayer();
+        ComputerPlayer secondPlayer = new ComputerPlayer();
+        for(int i=1;i<50;i++){
 
 
+
+        }
 
 
     }
