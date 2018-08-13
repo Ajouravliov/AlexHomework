f52536cf1978abd3905e1b4ff005128027559506
AJ <graudu@hotmail.com>
2018-08-14 01:56:25 +0300

    Gravetrips_06 Referee

--- src/Gravetrips/Referee.java
+++ src/Gravetrips/Referee.java
@@ -1,6 +1,44 @@
 package Gravetrips;
 
+import java.util.ArrayList;
+
 class Referee {
 
+    private boolean win;
 
+
+    private boolean getWin() {
+        return win;
+    }
+
+    private void setWin(boolean win) {
+        this.win = win;
+    }
+
+    public boolean checkWinner(ArrayList<String> lines) {
+
+        setWin(false);
+        for(String row:lines){
+
+           if((row.contains("xxxx")) || (row.contains("oooo"))){
+              setWin (true);
+           }
+        }
+
+        return getWin();
+    }
+
+    public char giveMove (int turnNumber){
+
+        if ((turnNumber%2) ==1){
+            return 'x';
+        } else {
+
+            return 'o';
+        }
+    }
+
+    public boolean isItLastMove(int turnNumber) {
+        return turnNumber == 49;
+    }
 }
